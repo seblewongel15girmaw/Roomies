5a5294a12d168099fe2341b820be0e8a3404614c
seblewongel15girmaw <seblina1224@gmail.com>
2023-12-30 10:36:56 -0800

    user endpoints

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,54 +22,27 @@ exports.getAllUsers = async (req, res) => {
 
 
 exports.registerUser = async (req, res) => {
-    try {
-      const {
-        full_name,
-        username,
-        email,
-        password,
-        gender,
-        age,
-        budget,
-        image,
-        personal_id,
-        bio,
-        phone_number,
-        address,
-        job_status,
-      } = req.body;
-  
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const userData = {
-        full_name,
-        username,
-        email,
-        password: hashedPassword,
-        gender,
-        age,
-        budget,
-        image,
-        personal_id,
-        bio,
-        phone_number,
-        address,
-        job_status,
-      };
-  
-      const user = new User();
-      const userId = await user.createUser(userData);
-  
-      res.status(201).json({ message: 'User registered successfully', userId });
-    } catch (error) {
-        
-        console.error('Error register users:', error);
-        res.status(500).json({ message: 'Internal server error', error: error.message });
-       
-      
-    }
-  };
+  try {
+    const {
+      full_name, username, email, password, gender, age, budget, image, personal_id, bio, phone_number, address, job_status,
+    } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userData = {
+      full_name, username, email,
+      password: hashedPassword, gender, age, budget, image, personal_id, bio, phone_number, address, job_status,
+    };
+
+    const user = new User();
+    const userId = await user.createUser(userData);
+
+    res.status(201).json({ message: 'User registered successfully', userId });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ message: 'Internal server error', error: error.message });
+  }
+};
 
 
 
--- models/userModel.js
+++ models/userModel.js
@@ -5,27 +5,35 @@ class User {
     const connection = await pool.getConnection();
 
     try {
+      const {
+        full_name, username, email, password, gender, age, budget, image, personal_id, bio, phone_number, address, job_status,
+      } = userData;
+
       const query = 'INSERT INTO users (full_name, username, email, password, gender, age, budget, image, personal_id, bio, phone_number, address, job_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+      const bindParams = [
+        full_name || null,
+        username || null,
+        email || null,
+        password || null,
+        gender || null,
+        age || null,
+        budget || null,
+        image || null,
+        personal_id || null,
+        bio || null,
+        phone_number || null,
+        address || null,
+        job_status || null,
+      ];
+
       console.log('SQL Query:', query);
-      const [result] = await connection.execute(query, [
-        userData.full_name,
-        userData.username,
-        userData.email,
-        userData.password,
-        userData.gender,
-        userData.age,
-        userData.budget,
-        userData.image,
-        userData.personal_id,
-        userData.bio,
-        userData.phone_number,
-        userData.address,
-        userData.job_status,
-      ]);
+      const [result] = await connection.execute(query, bindParams);
 
       return result.insertId;
     } catch (error) {
-      throw new Error('Error creating user');
+      console.error('Error creating user:', error);
+      throw error;
     } finally {
       connection.release();
     }

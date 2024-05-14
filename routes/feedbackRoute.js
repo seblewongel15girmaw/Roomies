f25fe95f6b675693d9cf021b854ba736f5d644f2
seblewongel <seblina1224@gmail.com>
2024-05-13 23:52:41 -0700

    feedback

--- routes/feedbackRoute.js
+++ routes/feedbackRoute.js
@@ -4,7 +4,4 @@ const feedbackController = require('../controllers/feedbackController');
 
 // Create a new feedback
 router.post('/add-feedback/:user_id', feedbackController.createFeedback);
-// Get all feedbacks
-// router.get('/', feedbackController.getAllFeedbacks);
-
 module.exports = router;
